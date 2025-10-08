86469f4e70c819a1c8f8adb35c44099500189cda
Grace_Kitonyi. <grace.kitonyi@aims.ac.rw>
2025-10-08 12:19:03 +0200

    Update password_checker_ui.py

--- password_checker_ui.py
+++ password_checker_ui.py
@@ -144,7 +144,7 @@ def check_password(password):
     if not has_digit:
         comments.append("🔸 Add at least one number.")
     if not has_special:
-        comments.append("🔸 Add at least one special character ('!@#$%ˆ&*()-_+=[]{};:’",<.>/?|‘ .)"')
+        comments.append("🔸 Add at least one special character (!@#$%ˆ&*()-_+=[]{};:’",<.>/?|‘ .)")
     if has_space:
         comments.append("🔸 Remove spaces — they weaken the password.")
 
